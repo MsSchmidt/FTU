7c13a52e70e72d5b2db550a934ed166c55b02e61
Carina <cs.carinaschmidt@gmail.com>
2018-03-18 17:52:09 +0100

    first commit

---
+++ R/test.R
@@ -0,0 +1,18 @@
+#' A test Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' test(2,3)
+
+install.packages("roxygen2")
+
+#devtools::install_github("klutometis/roxygen")
+#library(devtools)
+#has_devel()
+#library(testthat)
+#devtools::session_info()
+#devtools::install_github("hadley/devtools")
+#document()
